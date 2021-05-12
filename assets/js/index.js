adceb5716b2afdb0e64c431e52b7c0a50eb102f5
pablohariq <lizamav.p@gmail.com>
2021-05-12 16:13:42 -0400

    agregadas 2 de las 3 funciones js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,24 @@
+// const Swal = require('sweetalert2');
+
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   });
 //   inicializa el carrusel
 $('.carousel').carousel();
+
+// Enviar alerta convencional usando el botón de id #btnenviarCorreo
+// incluye una función anónima. De igual forma se podría crear esa función antes y pasarla como parámetro al metodo .click()
+$("#btnenviarCorreo").click(function() {
+  alert("El correo fue enviado correctamente.");
+})
+
+// Usando el mismo botón anterior usar alerta de Sweet Alert
+// en construcción
+
+// cambiador de color de titulos
+
+$("h1").on("dblclick", function(){ 
+  $(this).addClass("titulorojo");
+})
+  
